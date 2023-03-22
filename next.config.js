61984f8a3721575411b050647ecef59f6f7ca81d
Aryann208 <111970474+Aryann208@users.noreply.github.com>
2023-03-22 22:26:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'https://portfolio-website-sooty-alpha.vercel.app/',
-    ]
-  },
+  
 };
 
 module.exports = nextConfig;
